72cbb206ce0e33235efb73658bb3afe4ee23f74b
taurusilver7 <prasad20697@gmail.com>
2022-08-28 19:28:13 +0530

    dashboard cards styled

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { urlFor } from "../lib/sanity";
+import Tag from "./Tag";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ post }) => {
+  return (
+    <div className="card-container">
+      <h2>{post.title}</h2>
+      <p>Published on: {new Date(post.publishedAt).toDateString()}</p>
+
+      <img
+        src={urlFor(post.mainImage)}
+        alt="post_image"
+        className="main-image"
+      />
+      <hr />
+
+      <div className="info-container">
+        <p>Posted by: {post.username}</p>
+        <img src={urlFor(post.authorImage)} alt="author" className="avatar" />
+      </div>
+
+      <div className="tag-container">
+        {post.categories.map((cat, index) => (
+          <>{cat && <Tag key={index} title={cat.title} />}</>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- components/Tag.jsx
+++ components/Tag.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
-const Tag = () => {
+const Tag = ({ title }) => {
+  const getColor = () => {
+    let color;
+    switch (title.toLowerCase()) {
+      case "local":
+        color = "rgb(208, 125, 136)";
+        break;
+      case "tours":
+        color = "rgb(169, 210, 138)";
+        break;
+      case "fun":
+        color = "rgb(138, 173, 138)";
+        break;
+      default:
+        color = "rgb(128, 138, 210)";
+    }
+    return color;
+  };
   return (
-    <div>Tag</div>
-  )
-}
+    <div className="tag" style={{ backgroundColor: getColor() }}>
+      {title}
+    </div>
+  );
+};
 
-export default Tag
+export default Tag;
